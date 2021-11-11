d730c0a3bccc838991f25f591d3db92a605b821c
Vidya Akbar <vidyaakbar0608@gmail.com>
2021-11-11 10:11:12 +0530

    change in slice

--- entry/src/main/java/me/zhouzhuo/zzhorizontalprogressbardemo/slice/MainAbilitySlice.java
+++ entry/src/main/java/me/zhouzhuo/zzhorizontalprogressbardemo/slice/MainAbilitySlice.java
@@ -33,10 +33,8 @@ public class MainAbilitySlice extends AbilitySlice {
 
     @Override
     public void onStart(Intent intent) {
-
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
-
 
         final ZzHorizontalProgressBar pb = (ZzHorizontalProgressBar) findComponentById(ResourceTable.Id_pb);
         final ZzHorizontalProgressBar pb1 = (ZzHorizontalProgressBar) findComponentById(ResourceTable.Id_pb1);
@@ -46,20 +44,14 @@ public class MainAbilitySlice extends AbilitySlice {
         final ZzHorizontalProgressBar pb5 = (ZzHorizontalProgressBar) findComponentById(ResourceTable.Id_pb5);
         final ZzHorizontalProgressBar pb6 = (ZzHorizontalProgressBar) findComponentById(ResourceTable.Id_pb6);
 
-
-
         final Text tvProgress = (Text) findComponentById(ResourceTable.Id_tv_progress);
 
-
-
         Slider seekBar = (Slider) findComponentById(ResourceTable.Id_seekbar);
-
         seekBar.setValueChangedListener(new Slider.ValueChangedListener() {
 
             @Override
             public void onProgressUpdated(Slider slider, int progress, boolean b) {
                 try {
-
                     pb.setProgress(progress);
                     pb1.setProgress(progress);
                     pb1.setSecondProgress(progress - 20);
@@ -84,7 +76,6 @@ public class MainAbilitySlice extends AbilitySlice {
             @Override
             public void onTouchStart(Slider slider) {
                 //this method is empty
-
             }
 
             @Override
@@ -93,31 +84,23 @@ public class MainAbilitySlice extends AbilitySlice {
             }
         });
 
-
         try {
             pb4.setOnProgressChangedListener(new ZzHorizontalProgressBar.OnProgressChangedListener() {
 
                 @Override
                 public void onProgressChanged(ZzHorizontalProgressBar progressBar, int max, int progress) {
-
                     tvProgress.setText("Progress = " + progress + ", max = " + max);
-
                 }
 
                 @Override
                 public void onSecondProgressChanged(ZzHorizontalProgressBar progressBar, int max, int progress) {
                     //this method is empty
-
                 }
             });
         } catch (Exception e) {
             HiLog.debug(hiLogLabel,"null pointer exception");
         }
-
-
-
     }
-
 
     @Override
     public void onActive() {
